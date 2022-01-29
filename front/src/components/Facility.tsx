60a9067c5e0b902160af19fe90919c305f4ad432
y4shiro <y4shironao@gmail.com>
2022-01-29 15:19:52 +0900

    登録者と更新者の一覧を実装。日付表示には day.js を使用した

--- front/src/components/Facility.tsx
+++ front/src/components/Facility.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Container, Paper, TextField, makeStyles } from '@material-ui/core';
+import {
+  Avatar,
+  Chip,
+  Container,
+  InputLabel,
+  Paper,
+  TextField,
+  makeStyles,
+} from '@material-ui/core';
+import dayjs from 'dayjs';
 
 const useStyle = makeStyles((theme) => ({
   root: {
@@ -19,6 +28,16 @@ export const Facility: React.VFC = () => {
       <Paper className={style.paper}>
         <TextField label="設備名" fullWidth />
         <TextField label="詳細" fullWidth multiline />
+        <InputLabel shrink>登録者</InputLabel>
+        <p>
+          <Chip label="登録者名" avatar={<Avatar />}></Chip>
+          {dayjs(new Date()).format('YYYY-MM-DD HH:mm')}
+        </p>
+        <InputLabel shrink>更新者</InputLabel>
+        <p>
+          <Chip label="更新者名" avatar={<Avatar />}></Chip>
+          {dayjs(new Date()).format('YYYY-MM-DD HH:mm')}
+        </p>
       </Paper>
     </Container>
   );
