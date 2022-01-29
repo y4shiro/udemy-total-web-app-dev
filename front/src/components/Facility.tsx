985e67282bd15a54949fd89a854d7ddf7cbdb01c
y4shiro <y4shironao@gmail.com>
2022-01-29 11:53:40 +0900

    Facility.tsx にフォームを追加

--- front/src/components/Facility.tsx
+++ front/src/components/Facility.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Container, Paper, TextField, makeStyles } from '@material-ui/core';
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
 
 export const Facility: React.VFC = () => {
-  return <div>Hello Facility!</div>;
+  const style = useStyle();
+  return (
+    <Container maxWidth="sm" className={style.root}>
+      <Paper className={style.paper}>
+        <TextField label="設備名" fullWidth />
+        <TextField label="詳細" fullWidth multiline />
+      </Paper>
+    </Container>
+  );
 };
