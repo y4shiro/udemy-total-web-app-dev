d3ac461111ba2b9e4aa5c1b31a395003312922a1
y4shiro <y4shironao@gmail.com>
2022-01-30 18:22:02 +0900

    予約バーのコンポーネントを作成

---
+++ front/src/components/ReservationBar.tsx
@@ -0,0 +1,60 @@
+import React, { useMemo } from 'react';
+import { IReservation } from '../models/IReservation';
+import { Property } from 'csstype';
+import { makeStyles } from '@material-ui/styles';
+import { PropTypes, Theme } from '@material-ui/core';
+
+type PropsType = {
+  reservation: IReservation;
+  leftOffset: number;
+  beginHour: number;
+  hourWidth: number;
+  backgroundColor: Property.BackgroundColor;
+};
+
+type StyleType = {
+  width: number;
+  left: number;
+  backgroundColor: Property.BackgroundColor;
+};
+
+const useStyles = makeStyles<Theme, StyleType>(() => ({
+  root: {
+    height: '100%',
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    width: (p) => p.width + 'px',
+    left: (p) => p.left + 'px',
+  },
+  bar: {
+    height: '50%',
+    width: '100%',
+    backgroundColor: (p) => p.backgroundColor,
+  },
+}));
+
+export const ReservationBar: React.VFC<PropsType> = (props) => {
+  const { leftOffset, reservation, hourWidth, beginHour, backgroundColor } =
+    props;
+  const { startDate, endDate } = reservation;
+
+  const width = useMemo(() => {
+    const hours = endDate.diff(startDate, 'minutes') / 60;
+    return hourWidth * hours;
+  }, [startDate, endDate, hourWidth]);
+
+  const left = useMemo(() => {
+    const beginDate = startDate.set('hour', beginHour).startOf('hour');
+    const diffStart = startDate.diff(beginDate, 'minutes') / 60;
+    return leftOffset + diffStart * hourWidth;
+  }, [beginHour, hourWidth, leftOffset, startDate]);
+
+  const style = useStyles({ width, left, backgroundColor });
+
+  return (
+    <div className={style.root}>
+      <div className={style.bar}></div>
+    </div>
+  );
+};
