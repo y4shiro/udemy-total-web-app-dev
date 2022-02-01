af26c417713f7f2ab8e9e6829d77db7d240f8d4f
y4shiro <y4shironao@gmail.com>
2022-02-01 19:39:33 +0900

    Firebase プロジェクトを追加

---
+++ front/functions/.eslintrc.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [
+      path.join(__dirname, 'tsconfig.json'),
+      path.join(__dirname, 'tsconfig.dev.json'),
+    ],
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    quotes: ['error', 'double'],
+    'import/no-unresolved': 0,
+  },
+};
