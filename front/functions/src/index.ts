a0db410d41f4bce1f486d2784c4180761ad34d02
y4shiro <y4shironao@gmail.com>
2022-02-03 17:52:07 +0900

    Firebase プロジェクトに Express 導入

--- front/functions/src/index.ts
+++ front/functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from 'firebase-functions';
+import express = require('express');
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+app.get('/api', (req, res) => {
+  res.send('hello firebase.');
 });
+
+export const fn = functions.https.onRequest(app);
